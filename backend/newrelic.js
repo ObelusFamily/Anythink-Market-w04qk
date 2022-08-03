3e436f7c8907c72832d305e96ebb731ce100a850
John-Weak <tusharbali11@gmail.com>
2022-08-03 12:54:56 +0530

    urealted-history fix try

--- backend/newrelic.js
+++ backend/newrelic.js
@@ -18,6 +18,7 @@ exports.config = {
      */
     level: "info",
   },
+  
   /**
    * When true, all request headers except for those listed in attributes.exclude
    * will be captured for all traces, unless otherwise specified in a destination's
