1c2dca5a495ae7a3b3671dba5e26d5534cba1497
John-Weak <tusharbali11@gmail.com>
2022-08-03 19:45:50 +0530

    adds toggle for search bar

--- frontend/src/components/Home/Banner.js
+++ frontend/src/components/Home/Banner.js
@@ -1,8 +1,9 @@
-import React from "react";
+import React, { useState } from "react";
 import logo from "../../imgs/logo.png";
 import agent from "../../agent";
 
 const Banner = (props) => {
+  const [isSearchBarVisible, setIsSearchBarVisible] = useState(false);
   function searchInputChange(ev) {
     const title = ev.target.value.length > 2 ? ev.target.value : "";
     props.onSearchByTitle(
@@ -17,15 +18,25 @@ const Banner = (props) => {
       <div className="container p-4 text-center">
         <img src={logo} alt="banner" />
         <div>
-          <span id="get-part">A place to get</span>
-          <input
-            id="search-box"
-            type="search"
-            placeholder="What is that you truly desire?"
-            results="0"
-            className="item-search-title"
-            onChange={searchInputChange}
-          />
+          <span>A place to</span>
+          <span
+            style={{ cursor: "pointer" }}
+            id="get-part"
+            onClick={() => setIsSearchBarVisible(true)}
+          >
+            {" "}
+            get
+          </span>
+          {isSearchBarVisible && (
+            <input
+              id="search-box"
+              type="search"
+              placeholder="What is that you truly desire?"
+              results="0"
+              className="item-search-title"
+              onChange={searchInputChange}
+            />
+          )}
           <span> the cool stuff.</span>
         </div>
       </div>
